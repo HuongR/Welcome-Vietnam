d7eaaa42a33cc0ec61c380160696c6dea100670b
HuongR <dovanhuong1582003@gmail.com>
2025-06-04 23:03:36 +0700

    xoa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,6 @@ import AppRoutes from "./routes/index";
 import "./index.css"; // Import TailwindCSS
 
 function App() {
-  <h1>Hello world hihihihihihihihihihihi</h1>;
-  <h2>dcu m</h2>;
-  <h3>Dit cu huong</h3>
   return <AppRoutes />;
 }
 
